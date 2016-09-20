acb0844fc4c6a0e5b63e6aefe358e2ef1dec9200
陈欣 <95323362@qq.com>
2016-09-20 10:18:28 +0800

    -

---
+++ demo3_storyboard/demo3_storyboard/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  demo3_storyboard
+//
+//  Created by ChenXin on 16/9/20.
+//  Copyright © 2016年 ChenXin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ demo3_storyboard/demo3_storyboard/ViewController.m
@@ -0,0 +1,33 @@
+//
+//  ViewController.m
+//  demo3_storyboard
+//
+//  Created by ChenXin on 16/9/20.
+//  Copyright © 2016年 ChenXin. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (IBAction)unwindForHome:(UIStoryboardSegue *)unwindSegue towardsViewController:(UIViewController *)subsequentVC {
+    
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
